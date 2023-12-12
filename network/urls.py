ceafa29ffaf1c0802cd9eb0bd102ebd2a68b129f
absurdity69 <kfd@outlook.com>
2023-12-12 05:03:36 +0000

    +tidying up the files

--- network/urls.py
+++ network/urls.py
@@ -10,22 +10,15 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("profile", views.profile, name="profile"),
     path("userprofile/<int:user_id>", views.userprofile, name="userprofile"),
-    # API Routes
-    path("create", views.handle_create_form, name="handle_create_form"),
-    # Find a post by ID
-    path("posts/<int:post_id>", views.posts, name="posts"),
-    # View feed by user_id
-    path("feed/<int:user_id>", views.feed, name="feed"),
-    # Add/remove current user as a follower
-    path("addfollow/<int:user_id>", views.addfollow, name="addfollow"),
+    path("create", views.handle_create_form, name="handle_create_form"), # API Routes
+    path("posts/<int:post_id>", views.posts, name="posts"),     # Find a post by ID
+    path("feed/<int:user_id>", views.feed, name="feed"),     # View feed by user_id
+    path("addfollow/<int:user_id>", views.addfollow, name="addfollow"),     # Add/remove current user as a follower
     path("removefollow/<int:user_id>", views.removefollow, name="removefollow"),
-    # Following function and page
-    path("following", views.following, name="following"),
+    path("following", views.following, name="following"),     # Following function and page
     path("followingposts", views.followingposts, name="followingposts"),
-    # Like 
-    path('like/<int:post_id>', views.like, name='like'),
-    # Unlike 
-    path('unlike/<int:post_id>', views.unlike, name='unlike'),
-    # Edit post content 
-    path('edit/<int:post_id>', views.edit, name='edit'),
+    path('like/<int:post_id>', views.like, name='like'),     # Like 
+    path('unlike/<int:post_id>', views.unlike, name='unlike'),     # Unlike 
+    path('edit/<int:post_id>', views.edit, name='edit'),     # Edit post content 
+
 ]
